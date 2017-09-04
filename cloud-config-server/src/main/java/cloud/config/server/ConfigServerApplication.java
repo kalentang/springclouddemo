9b12da37d2d708624396751acdeb5dbc41faf7a5
Kalen Tang <kalentang@hotmail.com>
2017-09-04 17:28:59 +0800

    fix register-with-eureka bug

--- cloud-config-server/src/main/java/cloud/config/server/ConfigServerApplication.java
+++ cloud-config-server/src/main/java/cloud/config/server/ConfigServerApplication.java
@@ -1,15 +1,29 @@
 package cloud.config.server;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
+@RestController
 public class ConfigServerApplication 
 {
+	@Value("${spring.application.name}")
+    String name="world";
+    
+    @RequestMapping("/")
+    public String home(){
+    	return "Hello "+name;
+    }
+    
     public static void main( String[] args )
     {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
+    
+    
 }
